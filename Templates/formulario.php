83becce04663f00e257294874109b20d73dc3579
Raul Dionisio <rauldionisiosh@gmail.com>
2019-04-08 08:18:08 -0300

    Alteração do css dos inuputs e selects

--- Templates/formulario.php
+++ Templates/formulario.php
@@ -3,45 +3,51 @@
     <div class="form-row">
         <div class="form-group col-md-6">
             <label id="cidade" for="">Cidade:
-                <input type="text" name="cidade" class="form-control input1" id="" placeholder= "Insira sua cidade">
+                <input type="text" name="cidade" class="form-control" id="" placeholder= "Insira sua cidade">
             </label>
         </div>
          
-        <div class="form-group cool-md-6">      
+        <div class="form-group col-md-6">      
             <label for="">Bairro:
-                <input type="text" name="bairro" class="form-control input1" id="" placeholder= "Insira seu bairro">
+                <input type="text" name="bairro" class="form-control" id="" placeholder= "Insira seu bairro">
             </label>
         </div>
     </div> 
 
-    <div class="form-group">
-        <label for="">Dia:
-            <select name="dia" class="form-control">
-                <option value="" selected></option>
-                <option value="Domingo">Domingo</option>
-                <option value="Segunda">Segunda</option>
-                <option value="Terca">Terça</option>
-                <option value="Quarta">Quarta</option>
-                <option value="Quinta">Quinta</option>
-                <option value="Sexta">Sexta</option>
-                <option value="Sabado">Sábado</option>
-            </select>
-        </label>
-    </div>        
+
+  <div class="form-row">
+        <div class="form-group col selects">
+            <label for="selectDia">Dia:
+                <select name="dia" id="selectDia" class="form-control">
+                    <option value="" selected></option>
+                    <option value="Domingo">Domingo</option>
+                    <option value="Segunda">Segunda</option>
+                    <option value="Terca">Terça</option>
+                    <option value="Quarta">Quarta</option>
+                    <option value="Quinta">Quinta</option>
+                    <option value="Sexta">Sexta</option>
+                    <option value="Sabado">Sábado</option>
+                </select>
+            </label>
+    <!--   </div>        
 
 
-    <div class="form-group">        
-        <label for="">Hora:
-            <select name="horario" id="" class="form-control">
-                    <option value="" selected></option>  
-                <?php foreach ($horarios as $key => $value) : ?>
-                    <option value="<?php echo $value['horario']; ?>"><?php echo $value['horario']; ?></option>
-                <?php endforeach; ?>
-            </select>
-        </label> 
-    </div>
+        <div class="form-group col select"> -->        
+            <label id="hora" for="selectHora">Hora:
+                <select name="horario" id="selectHora" class="form-control">
+                        <option value="" selected></option>  
+                    <?php foreach ($horarios as $key => $value) : ?>
+                        <option value="<?php echo $value['horario']; ?>"><?php echo $value['horario']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </label> 
+        
 
-        <input type="submit" value="Buscar">
+        
 
+            <input class="btn btn-primary" type="submit" value="Buscar">
+        </div>
+
+     </div>       
     
 </form>
