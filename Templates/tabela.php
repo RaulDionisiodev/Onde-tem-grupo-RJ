1936bb91681b6831a176fdaa60349c9edc5d4036
Raul Dionisio <rauldionisiosh@gmail.com>
2019-04-12 08:59:16 -0300

    Melhoria na responsividade da tabela

--- Templates/tabela.php
+++ Templates/tabela.php
@@ -1,28 +1,34 @@
 <?php if($exibir_tabela) : ?>
-<table>
-    <tr>
-        <th>Nome</th>
-        <th>Paróquia</th>
-        <th>Bairro</th>
-        <th>Cidade</th>
-        <th>Dia</th>
-        <th>Horário</th>
-        <th></th>    
-    </tr>
-     
+<table class="table table-hover table-sm" >
+
+<caption>Lista de Grupos de oração</caption>
+
+    <thead>
+        <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Paróquia</th>
+            <th class="tabela-responsiva" scope="col" >Bairro</th>
+            <th class="tabela-responsiva" scope="col">Cidade</th>
+            <th scope="col" >Dia</th>
+            <th scope="col" >Horário</th>
+            <th scope="col" ></th>    
+        </tr>
+    </thead> 
+       
         <?php foreach($grupos as $key => $grupo ) : ?>    
+    <tbody>
+        <tr socpe="row">
+            <td><?php echo $grupo->nome;?></td>
+            <td><?php echo $grupo->paroquia;?></td>
+            <td class="tabela-responsiva" ><?php echo $grupo->bairro;?></td>
+            <td class="tabela-responsiva" ><?php echo $grupo->cidade;?></td>
+            <td><?php echo $grupo->dia;?></td>
+            <td><?php echo $grupo->horario;?></td>
+            <td><a href="templates/detalhes.php?id=<?php echo $grupo->id; ?>" rel="">Detalhes</a></td>
 
-    <tr>
-        <td><?php echo $grupo->nome;?></td>
-        <td><?php echo $grupo->paroquia;?></td>
-        <td><?php echo $grupo->bairro;?></td>
-        <td><?php echo $grupo->cidade;?></td>
-        <td><?php echo $grupo->dia;?></td>
-        <td><?php echo $grupo->horario;?></td>
-        <td><a href="templates/detalhes.php?id=<?php echo $grupo->id; ?>" rel="">Detalhes</a></td>
-
-        <?php endforeach; ?>
-    </tr>
+            <?php endforeach; ?>
+        </tr>
+    </tbody>
 </table>
 
 <?php else : ?>
